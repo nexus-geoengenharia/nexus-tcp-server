b75138d9cca89bc9129094cebdfbd537f2ccefc7
root <junior.alves.5@hotmail.com>
2019-06-18 21:00:15 +0000

    Fixing Server

--- server.js
+++ server.js
@@ -18,37 +18,37 @@ net
 
     // Send a nice welcome message and announce
     //socket.write("nexus243");
-    broadcast(socket.name + " joined the chat\n", socket);
+    broadcast("O hardware: "+socket.name + " estabeleceu uma conexao \n", socket);
 
     // Handle incoming messages from clients.
     socket.on("data", function(data) {
-      console.log(data.toString());
-      if (data.toString() === "nexus") {
+       console.log("Hardware enviou a palavra chave: "+data.toString()+"\n")
+       if (data.toString() === "nexus") {
         broadcast("nexus243", socket);
-        broadcast("140001", socket);
+        setTimeout(function(){broadcast("010400000001", socket);}, 10000);
       }
 
-      broadcast(socket.name + "> " + data, socket);
+      //broadcast(socket.name + "> " + data, socket+"\n");
     });
 
     // Remove the client from the list when it leaves
     socket.on("end", function() {
       clients.splice(clients.indexOf(socket), 1);
-      broadcast(socket.name + " left the chat.\n");
+      broadcast(socket.name + " Hardware desconectado.\n");
     });
 
     // Send a message to all clients
     function broadcast(message, sender) {
       clients.forEach(function(client) {
         // Don't want to send it to sender
-        if (client === sender) return;
+        //if (client === sender) return;
         client.write(message);
       });
       // Log it to the server output too
-      process.stdout.write(message);
+      process.stdout.write("Servidor nodeJS enviou: "+message+"\n");
     }
   })
   .listen(502);
 
 // Put a friendly message on the terminal of the server.
-console.log("Chat server running at port 502\n");
+console.log("Servidor nodeJS inicializado na porta 502\n");
